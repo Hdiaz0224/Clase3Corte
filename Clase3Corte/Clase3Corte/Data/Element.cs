a2dd44c7936ba8f665a235e5a108da08f076eb81
Hdiaz0224 <93561095+Hdiaz0224@users.noreply.github.com>
2022-05-28 20:17:28 -0500

    Añadiendo clase3corte
    Proyecto final

---
+++ Clase3Corte/Clase3Corte/Data/Element.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Clase3Corte.Data
+{
+    public class Libro
+    {
+        [Key]
+        public int IdLibro { get; set; }
+        public string Titulo { get; set; }
+        public string Editorial { get; set; }
+        public string Autor { get; set; }
+        public int Pagina { get; set; }
+        public string Descripcion { get; set; }
+    }
+    public class Estudiante
+    {
+        [Key]
+        public int IdEstudiante { get; set; }
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public string Carrera { get; set; }
+        public int Semestre { get; set; }
+        public string Descripcion { get; set; }
+
+    }
+}
